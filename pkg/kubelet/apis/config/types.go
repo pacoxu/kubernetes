a09f01dc8daf7403825544ea793ef29a6ee8b01b
haoyun <yun.hao@daocloud.io>
2021-10-20 16:52:00 +0800

    test: add e2e test for system-reserved swap doc: remove comma; fix: golangci-lint Signed-off-by: haoyun <yun.hao@daocloud.io>

--- pkg/kubelet/apis/config/types.go
+++ pkg/kubelet/apis/config/types.go
@@ -360,7 +360,7 @@ type KubeletConfiguration struct {
 
 	// A set of ResourceName=ResourceQuantity (e.g. cpu=200m,memory=150G,ephemeral-storage=1G,pid=100) pairs
 	// that describe resources reserved for non-kubernetes components.
-	// Currently only cpu, memory, local ephemeral storage for root file system, pid (process IDs), and swap are supported
+	// Currently only cpu, memory, local ephemeral storage for root file system, pid (process IDs) and swap are supported
 	// Since 1.22, swap is supported when NodeSwap is enabled
 	// See http://kubernetes.io/docs/user-guide/compute-resources for more detail.
 	SystemReserved map[string]string
